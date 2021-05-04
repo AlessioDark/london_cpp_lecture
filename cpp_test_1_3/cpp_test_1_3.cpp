7f7466f5830925267832f61b3feb353804adb905
maoxq <3288868269@qq.com>
2021-05-04 16:39:19 +0800

    获取最大值

---
+++ cpp_test_1_3/cpp_test_1_3.cpp
@@ -0,0 +1,18 @@
+﻿// cpp_test_1_3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int findMaxElement(vector<int>& v) {
+    return *(max_element(v.begin(), v.end()));
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
