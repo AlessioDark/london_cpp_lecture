d67943ef6cef485760df6f9c882dc231456ae3f2
maoxq <3288868269@qq.com>
2021-05-04 16:29:21 +0800

    考试题1—1

---
+++ cpp_test_1_1/cpp_test_1_1.cpp
@@ -0,0 +1,19 @@
+﻿// cpp_test_1_1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+int foo(int& a) {
+    a--;
+    return 10;
+}
+int main()
+{
+    int val = 7;
+    foo(val);
+    cout << val;
+}
+
+
