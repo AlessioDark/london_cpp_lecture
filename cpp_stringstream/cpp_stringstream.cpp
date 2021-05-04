69ee933fb6031521860aa2c1fd28885302800c07
maoxq <3288868269@qq.com>
2021-05-04 15:32:17 +0800

    stringstream的用法

---
+++ cpp_stringstream/cpp_stringstream.cpp
@@ -0,0 +1,18 @@
+﻿// cpp_stringstream.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    stringstream ss;
+    for (int i = 0; i != 100; ++i)
+        ss << "Hello,World...\n";
+    string str=ss.str();
+    cout << str << endl;
+}
+
